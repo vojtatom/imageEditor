387fbc630cebdf07aaf458bfebe725b37e0fe555
vojtatom <tomas@vojtatom.cz>
2016-12-30 19:22:39 +0100

    Loading and saving files
    Added features: saving file - work in progress
    
    Notes: Throws error while saving the image but the image is saved.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
  
 from tkinter import *
-from tkinter.filedialog import askopenfile
+from tkinter.filedialog import askopenfile, asksaveasfile
 from PIL import Image, ImageTk
 import numpy as np
 from time import sleep
@@ -13,7 +13,7 @@ class Application() :
 
         #setup text
         self.canvas_text = self.canvas.create_text(40, 90, anchor="nw")
-        self.canvas.itemconfig(self.canvas_text, text="this is the text")
+        self.canvas.itemconfig(self.canvas_text, text="click to load the image")
         self.canvas.bind("<Button-1>", self.canvas_click)
 
         self.image_loaded = False
@@ -36,6 +36,7 @@ class Application() :
         self.np_image = np.asarray(self.pillow_image, dtype=np.float)
         height, width, dim = self.np_image.shape
 
+        #resize if necessary
         if width > 1000 :
             basewidth = 1000
             wpercent = basewidth / width
@@ -50,20 +51,28 @@ class Application() :
         self.canvas.config(width=width, height=height)
         self.canvas.update()
 
+    def save_main_image(self, directory) :
+        self.pillow_image.save(directory)
+        self.canvas.delete(self.main_image)
+        self.canvas.config(width=500, height=200)
+        self.canvas.update()
+
     def run_application(self) :
         self.canvas.update()
         self.canvas.pack()
 
     def canvas_click(self, event) :
         if self.image_loaded == False :
-            directory = ask_open(self.file_opt)
+            directory = askopenfile(mode='r', **self.file_opt)
             print(directory)
             self.add_main_image(directory.name)
             self.image_loaded = True
+        else :
+            directory = asksaveasfile(mode='w', **self.file_opt)
+            print("OUTPUT", directory)
+            self.save_main_image(directory.name)
+            self.image_loaded = False
 
-
-def ask_open(file_opt) :
-    return askopenfile(mode='r', **file_opt)
 
 root = Tk()
 root.resizable(width=False, height=False)
